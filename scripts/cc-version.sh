26810f41d19e900a54de85f6ac9dea0878933cc8
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-04-14 12:15:07 -0500

    Revert silly changes I made to this script.

--- scripts/cc-version.sh
+++ scripts/cc-version.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-# Copyright (c) 2010-2011 Bryce Lelbach
-#
-# Distributed under the Boost Software License, Version 1.0.
-# (http://www.boost.org/LICENSE_1_0.txt).
+if [ "$1" = "-p" ] ; then
+  with_patchlevel=1;
+  shift;
+fi
 
-$@ -dumpversion
+compiler="$*"
+
+if [ ${#compiler} -eq 0 ]; then
+  echo "Error: No compiler specified."
+  printf "Usage:\n\t$0 <gcc-command>\n"
+  exit 1
+fi
+
+MAJOR=$(echo __GNUC__ | $compiler -E -xc - | tail -n 1)
+MINOR=$(echo __GNUC_MINOR__ | $compiler -E -xc - | tail -n 1)
+if [ "x$with_patchlevel" != "x" ] ; then
+  PATCHLEVEL=$(echo __GNUC_PATCHLEVEL__ | $compiler -E -xc - | tail -n 1)
+  printf "%02d%02d%02d\\n" $MAJOR $MINOR $PATCHLEVEL
+else
+  printf "%02d%02d\\n" $MAJOR $MINOR
+fi
 
